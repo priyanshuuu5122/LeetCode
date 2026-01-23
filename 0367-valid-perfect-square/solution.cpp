c49c28685d6be0f0e2084568379184691e994611
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-01-23 06:06:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (53.73%)

---
+++ 0367-valid-perfect-square/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+        int odd = 1;
+
+        while (num > 0) {
+            num -= odd;
+            odd += 2;
+        }
+
+        return num == 0;
+    }
+};
+

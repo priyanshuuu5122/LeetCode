7d71cb9e0000557fba0e9a151d84cf060f80c022
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-01-31 07:19:29 +0000

    [LeetCode Sync] Runtime - 3 ms (18.65%), Memory - 193.5 MB (54.23%)

---
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+
+        int n = nums.size(); 
+        int res = 0;
+
+        for(int i=n-1;i>0;i--){
+            if(nums[i]<=nums[i-1]){
+                res = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+};
+
+
+

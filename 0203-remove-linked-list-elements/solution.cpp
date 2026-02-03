01de1a55ae0338495141da46c1198dc72e5dec42
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-02-03 20:22:22 +0000

    [LeetCode Sync] Runtime - 1 ms (17.34%), Memory - 20.3 MB (12.47%)

---
+++ 0203-remove-linked-list-elements/solution.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* current = dummy;
+        
+        while (current->next != nullptr) {
+            if (current->next->val == val) {
+                ListNode* toDelete = current->next;
+                current->next = current->next->next;
+                delete toDelete; // Important: free memory in C++
+            } else {
+                // Move to next node
+                current = current->next;
+            }
+        }
+        
+        ListNode* newHead = dummy->next;
+        delete dummy; // Don't forget to delete dummy node
+        return newHead;
+    }
+};

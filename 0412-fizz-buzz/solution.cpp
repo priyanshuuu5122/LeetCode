bed49e57f8d12b3947dfca854429b5e5e9d085a8
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-02-07 10:23:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.6 MB (73.28%)

--- 0412-fizz-buzz/solution.cpp
+++ 0412-fizz-buzz/solution.cpp
@@ -1,26 +1,57 @@
+// class Solution {
+// public:
+//     vector<string> fizzBuzz(int n) {
+
+//         vector<string> ans;
+
+//         for (int i = 1; i <= n; i++) {
+
+//             if (i % 3 == 0 && i % 5 == 0) {
+//                 ans.push_back("FizzBuzz");
+//             }
+//             else if (i % 3 == 0) {
+//                 ans.push_back("Fizz");
+//             }
+//             else if (i % 5 == 0) {
+//                 ans.push_back("Buzz");
+//             }
+//             else {
+//                 ans.push_back(to_string(i));
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
-
-        vector<string> ans;
+        vector<string> result;
 
         for (int i = 1; i <= n; i++) {
 
-            if (i % 3 == 0 && i % 5 == 0) {
-                ans.push_back("FizzBuzz");
+            if (i % 3 == 0 && i % 5 == 0)  { 
+                result.push_back("FizzBuzz");
             }
-            else if (i % 3 == 0) {
-                ans.push_back("Fizz");
-            }
-            else if (i % 5 == 0) {
-                ans.push_back("Buzz");
-            }
-            else {
-                ans.push_back(to_string(i));
+
+            else if (i % 3 == 0) { 
+                result.push_back("Fizz");
+            } 
+
+            else if (i % 5 == 0) { 
+                result.push_back("Buzz");
+            } 
+ 
+            else { 
+                result.push_back(to_string(i));
             }
         }
 
-        return ans;
+        return result;
     }
 };
+
+
 

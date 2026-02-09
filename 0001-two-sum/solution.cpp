b6fa656cf2f51f741fa99c3c6013a0f947d22681
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-02-09 08:19:21 +0000

    [LeetCode Sync] Runtime - 3 ms (66.68%), Memory - 15.1 MB (12.22%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,15 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             for (int j = i + 1; j < nums.size(); j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+
+//         return {-1, -1};
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mp;  // value -> index
 
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        for(int i = 0; i < nums.size(); i++) {
+            int left = target - nums[i];
+
+            // check: kya required value pehle aa chuki hai?
+            if(mp.find(left) != mp.end()) {
+                return { mp[left], i };
             }
-        }
 
+            // current value ko map me store karo
+            mp[nums[i]] = i;
+        }
         return {-1, -1};
     }
 };

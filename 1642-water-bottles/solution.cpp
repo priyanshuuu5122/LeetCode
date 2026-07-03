acfbac289162638db1e1fc9b8a32a04c080926e2
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-07-03 10:23:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (7.15%)

---
+++ 1642-water-bottles/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+
+        int ans = numBottles;
+        int empty = numBottles;
+
+        while (empty >= numExchange) {
+
+            int newBottles = empty / numExchange;
+            ans += newBottles;
+
+            empty = (empty % numExchange) + newBottles;
+        }
+
+        return ans;
+    }
+};

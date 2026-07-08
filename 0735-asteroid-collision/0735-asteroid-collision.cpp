7a6fef12ba111fa7660aa6ac5a16c57dae09b053
Priyanshu Raj <146623988+priyanshuuu5122@users.noreply.github.com>
2026-07-08 15:28:30 +0530

    Time: 0 ms (100%), Space: 22.2 MB (19.09%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        stack<int> st;
+
+        for (int a : asteroids) {
+
+            bool destroyed = false;
+
+            while (!st.empty() && a < 0 && st.top() > 0) {
+
+                if (st.top() < -a) {
+
+                    st.pop();
+                    continue;
+                }
+
+                else if (st.top() == -a) {
+
+                    st.pop();
+                }
+
+                destroyed = true;
+                break;
+            }
+
+            if (!destroyed)
+                st.push(a);
+        }
+
+        vector<int> ans(st.size());
+
+        for (int i = st.size() - 1; i >= 0; i--) {
+
+            ans[i] = st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
